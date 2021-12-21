64c6631e91d079f15288bfd2191a843cdc5b9819
Salva597 <salgonger1997@gmail.com>
2021-12-21 23:24:24 +0100

    Added templates and tests

--- Classes/TDGenerator.py
+++ Classes/TDGenerator.py
@@ -12,30 +12,30 @@ class TDGenerator:
     def __init__(self, id):
         self.id = id
         self.tds = []
-        self.sparql_query = None
-        self.query_results = None
+        self.sparql_query = 'select distinct ?s where {[] a ?s} LIMIT 100' # Change to None if passed as parameter
+        self.query_results = []
         self.template = None
-        self.render_info = None
-        self.search_template_path = 'templates/'
+        self.render_info = {}
+        #self.search_template_path = 'templates/'
         self.template_name = 'td.template.txt'
         self.td_directory_endpoint = "http://cogito-thing-manager_wothive_1:9000/api/things/{}".format(id) # We can take the id from the subjects obtained by the query
         self.response = {"tds":[]}
 
-    def generate_td(self, rendering_info):
+    def generate_td(self):
         """
         Generate Thing Descriptions by rendering templates
         """
-        template_rendered = self.template.render(rendering_info) # rendered in str format
+        template_rendered = self.template.render(self.render_info) # rendered in str format
         template_rendered = re.sub(r"[\n\t\s]*", "", template_rendered).replace(",}", "}").replace(",]", "]") # preprocess string before rendering to json format
         # print(template_rendered)
         td = json.dumps(json.loads(template_rendered), indent=4) # rendered in json format and inedented
         self.tds.append(td)
 
-    def load_template(self):
+    def load_template(self, search_template_path):
         """
         Loads the template to create TDs
         """
-        searchpath = [self.search_template_path] # search path
+        searchpath = [search_template_path] # search path
         engine = Engine(
             loader=FileLoader(searchpath),
             extensions=[CoreExtension()]
@@ -46,21 +46,45 @@ class TDGenerator:
         """
         Query triple store to retrieve elements to create TDs
         """
-        sparql = SPARQLWrapper("http://virtuoso_db:8890/sparql")
+        sparql = SPARQLWrapper("http://localhost:8890/sparql") # change to virtuoso_db
+        self.sparql_query.encode('utf-8')
         sparql.setQuery(self.sparql_query)
 
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
 
         for result in results["results"]["bindings"]:
-            self.query_results += result["s"]["value"]
+            self.query_results.append(result["s"]["value"])
 
 
-    def info_to_render(self):
+    def info_to_render(self): # add subject as parameter
         """
         Retrieve info in a json format to render the template
         """
-        pass
+        self.render_info.update(
+            {
+                "prefixes": [
+                    {"name":"beo", "url":"https://pi.pauwel.be/voc/buildingelement#"} # prefixes
+                ],
+                "id":self.id,
+                "types": [
+                    "bot:Building" # subject
+                ],
+                "description":"A building element.", # description about the subject
+                "properties": [
+                    {
+                        "name": "IFC", # type of file
+                        "uri": "https://example_uri.com/files/cogito1234.ifc", # uri of the file
+                        "type": "application/ifc" # type_name of the file
+                    },
+                    {
+                        "name": "KnowledgeGraph", # graph
+                        "uri": "https://openmetrics.eu/openmetrics#Building_120", # uri of the graph
+                        "type": "text/turtle" # type_name of the graph
+                    }
+                ]
+            }
+        )
 
 
     def store_TD(self):
@@ -80,7 +104,7 @@ class TDGenerator:
         # Call self.query_triple_store() to query triple store
         # Call self.template_to_render() to load the template
         # Inside for loop
-            # Call info_to_render() to retrieve info in a json format to render the template
+            # Call info_to_render(s) to retrieve info in a json format to render the template
             # Call generate_td() to generate TDs
         # Call store_TD() to store TDs in Thing Directory
         pass
